0f96c95d685dc0188543d137f1b2bdb84bcaf066
sireesha2000 <60355132+sireesha2000@users.noreply.github.com>
2020-03-09 22:31:24 +0530

    Add files via upload

---
+++ task7/epam7/src/main/java/Creational/SingletonPattern.java
@@ -0,0 +1,17 @@
+package Creational;
+class SingleObject {
+	   private static SingleObject instance = new SingleObject();
+	   private SingleObject(){}
+	   public static SingleObject getInstance(){
+	      return instance;
+	   }
+	   public void showMessage(){
+	      System.out.println("Hello World!");
+	   }
+	}
+public class SingletonPattern {
+	public static void main(String[] args) {
+	      SingleObject object = SingleObject.getInstance();
+	      object.showMessage();
+	   }
+}

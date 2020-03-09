966cabcba47a00a365b1632d4ce8df827d922f69
sireesha2000 <60355132+sireesha2000@users.noreply.github.com>
2020-03-09 22:27:29 +0530

    Create IteratorPattern.java

---
+++ task7/epam7/src/main/java/Behavioral/IteratorPattern.java
@@ -0,0 +1,46 @@
+package Behavioral;
+interface Iterator {
+	   public boolean hasNext();
+	   public Object next();
+	}
+interface Container {
+	   public Iterator getIterator();
+	}
+class NameRepository implements Container {
+	   public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
+
+	   public Iterator getIterator() {
+	      return new NameIterator();
+	   }
+
+	   private class NameIterator implements Iterator {
+
+	      int index;
+
+	      public boolean hasNext() {
+	      
+	         if(index < names.length){
+	            return true;
+	         }
+	         return false;
+	      }
+
+	      public Object next() {
+	      
+	         if(this.hasNext()){
+	            return names[index++];
+	         }
+	         return null;
+	      }		
+	   }
+	}
+public class IteratorPattern {
+	public static void main(String[] args) {
+	      NameRepository namesRepository = new NameRepository();
+
+	      for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+	         String name = (String)iter.next();
+	         System.out.println("Name : " + name);
+	      } 	
+	   }
+}
